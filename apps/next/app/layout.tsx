ee502da270e16e63b10b2d8d3bf497608b0b2478
Mike Allen <mikefacesny@gmail.com>
2024-05-01 02:22:51 -0400

    add favicon

--- apps/next/app/layout.tsx
+++ apps/next/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="#" />
         <StyledJsxRegistry>
           <Provider mode="light">{children}</Provider>
         </StyledJsxRegistry>
